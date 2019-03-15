a6b640b144027161307b651e61976b4b86220e0a
Dan Gale <danjgale@gmail.com>
2019-03-14 21:45:01 -0400

    initial commit

---
+++ nitrial/estimator.py
@@ -0,0 +1,11 @@
+
+class TrialEstimator(object):
+
+    def __init__(self, model, events, separate=True):
+        pass
+
+    def fit(self):
+        pass
+
+        self.design_matrix_ = None
+        self.estimate_ = None
